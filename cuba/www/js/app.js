fcefb241e80b3e061f7091790aa247c6bc102748
3nGercog <sereqa0813@rambler.ru>
2016-07-07 22:22:58 +0300

    change login

--- cuba/www/js/app.js
+++ cuba/www/js/app.js
@@ -23,51 +23,25 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+    $stateProvider
 
-    
-    .state('login', {
-        url: '/login',
-        templateUrl: 'templates/login.html',
-        controller: 'EnterController'
-    })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
+      .state('login', {
+          url: '/login',
+          templateUrl: 'templates/login.html',
+          controller: 'EnterController'
+      })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+      .state('app.playlists', {
+          url: '/playlists',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/playlists.html',
+                  controller: 'PlaylistsCtrl'
+              }
+          }
+      });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/login');
 });
